dc82ebece2abbb71b94cc777fd22d630b3f7aebe
JonaPlaz <jplazanet@hotmail.com>
2024-10-10 15:40:51 +0200

    Add dotenv configuration and update hardhat.config.js

--- 3. Outils Dev/Hardhat/hardhat.config.js
+++ 3. Outils Dev/Hardhat/hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv/config");
+require("@nomicfoundation/hardhat-verify");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const HOLESKY_RPC_URL = process.env.HOLESKY_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_ID = process.env.ETHERSCAN_API_ID || "";
+
 module.exports = {
-  solidity: "0.8.28",
+  defaultNetwork: "hardhat",
+  networks: {
+    holesky: {
+      url: HOLESKY_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 17000,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_ID
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+      }
+    ]
+  },
 };
